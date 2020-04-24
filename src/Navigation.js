67d67dd0a8802feee3d0f1c56f2f7844350f96fc
Leopoldo Garcia <legarbo@gmail.com>
2020-04-24 09:02:15 -0400

    Create Hamburger Component as class

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Navigation.css';
+import Hamburger from './Hamburger';
 
 function Navigation(props) {
   return (
     <div>
-      <ul className="topnav">
-        <li><a className="active" href="#home">Home</a></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li className="right"><a href="#about">About</a></li>
-      </ul>
-        <h1 className="heading">Let Us Master<span>This!!!</span></h1>
+      <div className="nav">
+        <div className="nav-header">
+          <Hamburger />
+        </div>
+      </div>
+      <h1 className="heading">Let Us Master<span>This!!!</span></h1>
     </div>
   )
 }
