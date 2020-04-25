87d143fb879f9c09c19b0dab48a2707d7e1b1c3a
Leopoldo Garcia <legarbo@gmail.com>
2020-04-24 22:36:40 -0400

    Finish styling Nav && make Navigation a classComponent

--- src/Hamburger.js
+++ src/Hamburger.js
@@ -1,50 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Hamburger.css';
 import classNames from 'classnames';
 
-class Hamburger extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { transformed: false , styles: ''};
-    this.handleClick = this.handleClick.bind(this);
-  }
+function Hamburger({transformed, onClick, styles}) {
 
-  handleClick() {
-    if (this.state.transformed) {
-      this.setState({
-        transformed: false,
-        styles: 'appear 350ms ease-in forwards'
-      }) 
-    } else {
-      this.setState({
-        transformed: true,
-        styles: 'disappear 350ms ease-in forwards'
-      })
-    }
-  }
-
-  render() {
     let hambButton = classNames({
       'hamburger': true,
-      'transformed': this.state.transformed
+      'transformed': transformed
     });
     let secondLine = classNames({
       'line': true,
-      'secondLine': this.state.transformed
+      'secondLine': transformed
     });
 
     return (
       <div>
-        <div className={hambButton} onClick={this.handleClick}>
+        <div className={hambButton} onClick={onClick}>
             <div className="line"></div>
             <div className={secondLine}
-                style={{animation: this.state.styles}}>
+                style={{animation: styles}}>
             </div>
             <div className="line"></div>
         </div>
       </div>
     )
-  }
 }
 
 export default Hamburger;
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React,{ Component } from 'react';
+import { FaHome, FaBriefcase, FaQuestion } from 'react-icons/fa';
+import { MdContactMail} from 'react-icons/md';
 import './Navigation.css';
 import Hamburger from './Hamburger';
 
-function Navigation(props) {
-  return (
-    <div>
-      <div className="nav">
-        <div className="nav-header">
-          <Hamburger />
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { transformed: false, styles: ''};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    if (this.state.transformed) {
+      this.setState({
+        transformed: false,
+        styles: 'appear 350ms ease-in forwards'
+      })
+    } else {
+      this.setState({
+        transformed: true,
+        styles: 'disappear 350ms ease-in forwards'
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="nav">
+            <Hamburger 
+              className="hamburger"
+              transformed={this.state.transformed} 
+              styles={this.state.styles} 
+              onClick={this.handleClick}
+            />
+            <div className="logo">LeGarbo</div>
+            { this.state.transformed
+            ? <div className="nav-links">
+              <div><a href="#home"><FaHome /></a></div>
+                <div><a href="#portfolio"><FaBriefcase /> Portfolio</a></div>
+                  <div><a href="#contact"><MdContactMail /></a></div>
+                    <div><a href="#faq"><FaQuestion /></a></div>  
+            </div>
+                : null
+            }
         </div>
+        <h1 className="heading">Let Us Master<span>This!!!</span></h1>
       </div>
-      <h1 className="heading">Let Us Master<span>This!!!</span></h1>
-    </div>
-  )
+    )
+  }
 }
 
 export default Navigation;
-
-
